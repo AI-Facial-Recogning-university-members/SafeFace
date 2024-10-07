b542ac110244581f43aa8ba473b32e99e2f6614c
Noah Vêri Franco <noahvf16@outlook.com>
2024-10-07 15:43:17 -0300

    Criação do database com mysqlite, utilizando poo

---
+++ src/database.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+#função para conectar ao banco de dados e criar uma tabela de usuarios caso nao exista.
+def create_db():
+    conn = sqlite3.connect('funcionarios.db')
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS funcionarios (id INTEGER PRIMARY KEY AUTOINCREMENT,nome text NOT NULL, cpf TEXT NOT NULL UNIQUE, dt_nascimento TEXT NOT NULL, imagem_path TEXTO NOT NULL))")
+
+    conn.commit()
+    conn.close()
+
+#função para adicionar um funcionario
+def add_funcionario(nome, cpf, dt_nascimento, imagem_path):
+    conn = sqlite3.connect('funcionarios.db')
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO funcionarios (nome,cpg,dt_nascimento, imagem_path) VALUES (?,?,?,?)",(nome, cpf, dt_nascimento,imagem_path))
+
+    conn.commit()
+    conn.close()
+
+#função para remover um funcionario pela busca por cpf
+def remover_funcionario(cpf):
+    conn = sqlite3.connect('funcionarios.db')
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM funcionarios WHERE cpf = ?", (cpf))
+    conn.commit()
+    conn.close()
+
+#função para retornar todos os funcionarios
+def get_all_funcionarios():
+    conn = sqlite3.connect('funcionarios.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM funcionarios")
+    funcionarios = cursor.fetchall()
+
+    conn.close()
+    return funcionarios
