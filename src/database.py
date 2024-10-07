fa7d98182f01422ff0c487f249db0222df96f34f
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2024-10-07 20:09:09 -0300

    fix: fixing problems related to table fields

--- src/database.py
+++ src/database.py
@@ -1,41 +1,35 @@
 import sqlite3
 
-# função para conectar ao banco de dados e criar uma tabela de usuarios caso nao exista.
-def create_db() -> None:
+#função para conectar ao banco de dados e criar uma tabela de usuarios caso nao exista.
+def connect_db() -> sqlite3.Connection:
     conn = sqlite3.connect('funcionarios.db')
-    cursor = conn.cursor()
+    return conn
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS funcionarios (id INTEGER PRIMARY KEY AUTOINCREMENT,nome text NOT NULL, cpf TEXT NOT NULL UNIQUE, dt_nascimento TEXT NOT NULL, imagem_path TEXTO NOT NULL))")
+def create_table() -> None:
+    conn = connect_db()
+    curr = conn.cursor()
+
+    curr.execute("""
+        CREATE TABLE IF NOT EXISTS funcionarios_tbl (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome VARCHAR(32),
+            cpf VARCHAR(14),
+            dt_nascimento TEXT
+        )
+    """)
 
     conn.commit()
+    curr.close()
     conn.close()
 
-# função para adicionar um funcionario
-def add_funcionario(nome, cpf, dt_nascimento, imagem_path) ->  None:
-    conn = sqlite3.connect('funcionarios.db')
-    cursor = conn.cursor()
+#função para adicionar um funcionario
+def add_funcionario(nome, cpf, dt_nascimento):
+    raise NotImplementedError
 
-    cursor.execute("INSERT INTO funcionarios (nome,cpg,dt_nascimento, imagem_path) VALUES (?,?,?,?)",(nome, cpf, dt_nascimento,imagem_path))
-
-    conn.commit()
-    conn.close()
-
-# função para remover um funcionario pela busca por cpf
-def remover_funcionario(cpf) -> None:
-    conn = sqlite3.connect('funcionarios.db')
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM funcionarios WHERE cpf = ?", (cpf))
-    conn.commit()
-    conn.close()
+#função para remover um funcionario pela busca por cpf
+def remover_funcionario(cpf):
+    raise NotImplementedError
 
 #função para retornar todos os funcionarios
-def get_all_funcionarios() -> None:
-    conn = sqlite3.connect('funcionarios.db')
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM funcionarios")
-    funcionarios = cursor.fetchall()
-
-    conn.close()
-    return funcionarios
+def get_all_funcionarios():
+    raise NotImplementedError
