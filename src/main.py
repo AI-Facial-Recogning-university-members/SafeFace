bbc4d86bbd1a56e961aa3962500925ce8b77e5d0
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-10-17 15:07:07 -0300

    refactor: separated the code into functions

--- src/main.py
+++ src/main.py
@@ -3,60 +3,75 @@ import mediapipe as mp
 import face_recognition
 import numpy as np
 
-# Inicializando MediaPipe e OpenCV
-webcam = cv2.VideoCapture(0)
-reconhecimento_rosto = mp.solutions.face_detection
-desenho = mp.solutions.drawing_utils
-reconhecedor_rosto = reconhecimento_rosto.FaceDetection()
 
 # Carregando imagens e nomes
 conhecidos_encodings = []
 nomes = []
 
+def desenhar_retangulo_rosto(frame, local_rosto, nome) -> None:
+	# Desenhando o retângulo ao redor do rosto e o nome
+	top, right, bottom, left = local_rosto
+	cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+	cv2.putText(frame, nome, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255))
+
+def desenhar_rosto(frame, desenho, rosto) -> None:
+	# Desenhando o rosto detectado
+	desenho.draw_detection(frame, rosto)
+
+	# Obtendo localizações dos rostos no frame atual
+	face_locations = face_recognition.face_locations(frame)
+	face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+	for face_encoding, face_location in zip(face_encodings, face_locations):
+		# Comparando rostos detectados com rostos conhecidos
+		matches = face_recognition.compare_faces(conhecidos_encodings, face_encoding)
+		name = "Desconhecido"
+
+		face_distances = face_recognition.face_distance(conhecidos_encodings, face_encoding)
+		best_match_index = np.argmin(face_distances)
+
+		if matches[best_match_index]:
+			name = nomes[best_match_index]
+
+	desenhar_retangulo_rosto(frame, face_locations, nome)
+
 def carregar_imagem(nome_arquivo, nome):
-    imagem = face_recognition.load_image_file(nome_arquivo)
-    encoding = face_recognition.face_encodings(imagem)[0]
-    conhecidos_encodings.append(encoding)
-    nomes.append(nome)
+ 	imagem = face_recognition.load_image_file(nome_arquivo)
+ 	encoding = face_recognition.face_encodings(imagem)[0]
+ 	conhecidos_encodings.append(encoding)
+ 	nomes.append(nome)
 
-# Adicione suas imagens aqui
-carregar_imagem("./img/gustavo.jpg", "Gustavo")
-carregar_imagem("./img/leoo.jpg", "Leonardo")
+def main() -> int:
+	# Inicializando MediaPipe e OpenCV
+	webcam = cv2.VideoCapture(0)
+	desenho = mp.solutions.drawing_utils
+	reconhecimento_rosto = mp.solutions.face_detection
+	reconhecedor_rosto = reconhecimento_rosto.FaceDetection()
 
-while webcam.isOpened():
-    validacao, frame = webcam.read()
-    if not validacao:
-        break
-    imagem_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    lista_rostos = reconhecedor_rosto.process(imagem_rgb)
+	carregar_imagem("./img/gustavo.jpg", "Gustavo")
+	carregar_imagem("./img/leoo.jpg", "Leonardo")
 
-    if lista_rostos.detections:
-        for rosto in lista_rostos.detections:
-            # Desenhando o rosto detectado
-            desenho.draw_detection(frame, rosto)
-            
-            # Obtendo localizações dos rostos no frame atual
-            face_locations = face_recognition.face_locations(frame)
-            face_encodings = face_recognition.face_encodings(frame, face_locations)
-            
-            for face_encoding, face_location in zip(face_encodings, face_locations):
-                # Comparando rostos detectados com rostos conhecidos
-                matches = face_recognition.compare_faces(conhecidos_encodings, face_encoding)
-                name = "Desconhecido"
-                
-                face_distances = face_recognition.face_distance(conhecidos_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = nomes[best_match_index]
+	while(webcam.isOpened()):
+		validacao, frame = webcam.read()
 
-                # Desenhando o retângulo ao redor do rosto e o nome
-                top, right, bottom, left = face_location
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-                cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+		if not validacao:
+			break
 
-    cv2.imshow("Rostos na sua webcam", frame)
-    if cv2.waitKey(5) == 27:  # ESC
-        break
+		imagem_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		lista_rostos = reconhecedor_rosto.process(imagem_rgb)
 
-webcam.release()
-cv2.destroyAllWindows()
+		if lista_rostos.detections:
+			for rosto in lista_rostos.detections:
+				desenhar_rosto(frame, desenho, rosto)
+
+		cv2.imshow("Rostos na sua webcam", frame)
+		if cv2.waitKey(5) == 27:  # ESC
+			break
+
+	webcam.release()
+	cv2.destroyAllWindows()
+
+	return 0
+
+if(__name__ == "__main__"):
+	main()
