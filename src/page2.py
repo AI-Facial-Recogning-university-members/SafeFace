a5e03f6876107b84ac81826f7a7ebdfab503af0b
Noah Vêri Franco <146153343+DevNoahF@users.noreply.github.com>
2024-10-22 21:03:53 -0300

    adicionei para receber o que foi escrito, falta joao aplicar o banco de dados

--- src/page2.py
+++ src/page2.py
@@ -2,9 +2,32 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.style import Style
 import subprocess 
+import sqlite3
 
-def cadastrar():
-    pass
+def salvar_nome(nome):
+    conn=sqlite3.connect('database.py')
+    cursor=conn.cursor()
+
+    cursor.execute('''
+                    ''')
+    
+def validar_nome(x)-> bool:
+    if x.isdigit():
+        return True
+    elif x == "":
+        return True
+    else:
+        return False
+    
+def validar_cpf(x)-> bool:
+    if x.isdigit():
+        return False
+    elif x == "":
+        return True
+    else:
+        return True
+
+
 
 def voltar():
     subprocess.run(["python", "page1.py"])
@@ -19,16 +42,21 @@ label = ttk.Label(app, text="Cadastro")
 label.config(font=("Arial", 20, "bold"))
 label.pack(pady=35)
 
+digitNome_func= app.register(validar_nome)
+digitCpf_func= app.register(validar_cpf)
+
 # Campo Nome
 nome = ttk.Frame(app)
 nome.pack(pady=18, padx=10, fill="x")
 ttk.Label(nome, text="Nome").pack(side=LEFT, padx=5)
+nome_entry = ttk.Entry(app, validate="focus", validatecommand=(digitNome_func, '%P'))
 ttk.Entry(nome).pack(side=LEFT, fill="x", expand=True, padx=5)
 
 # Campo CPF
 cpf = ttk.Frame(app)
 cpf.pack(pady=18, padx=10, fill="x")
 ttk.Label(cpf, text="CPF").pack(side=LEFT, padx=10)
+cpf_entry = ttk.Entry(app, validate="focus", validatecommand=(digitCpf_func, '%P'))
 ttk.Entry(cpf).pack(side=LEFT, fill="x", expand=True, padx=5)
 
 
